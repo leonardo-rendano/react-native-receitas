40c8785d37650aaeeeffcaa7ec6d37a2b6fbbf28
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-04-03 13:42:25 -0300

    routes config

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native/types";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Search() {
   return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,51 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"; 
-import Home from "../pages/Home";
+import Home from "../pages/home";
 import Favourites from "../pages/favourites";
+import { Ionicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
 
 export function Routes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeTab" component={Home} />
-      <Tab.Screen name="Favourites" component={Favourites} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#121212",
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 0
+        }
+      }}
+    >
+      <Tab.Screen
+        name="HomeTab"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Ionicons name="home" color="#000" size={size} />
+            }
+
+            return <Ionicons name="home-outline" color={color} size={size} />
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Favourites"
+        component={Favourites}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Ionicons name="heart" color="#ff4141" size={size} />
+            }
+
+            return <Ionicons name="heart-outline" color={color} size={size} />
+          }
+        }}
+      />
     </Tab.Navigator>
   )
 }
