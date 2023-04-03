954fb291375fe1ed0cb1588a73bfef930ec441d2
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-04-03 14:10:43 -0300

    home styles

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,67 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { FormEvent, useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
+import { Ionicons } from '@expo/vector-icons'
+import { Logo } from "../../components/Logo";
 
 export default function Home() {
+  const [inputValue, setInputvalue] = useState<string>()
+
+  function handleSearch(): void {
+    alert(`Você digitou ${inputValue}`)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Página Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Logo />
+
+      <Text style={styles.title}>Encontre a receita</Text>
+      <Text style={styles.title}>que combina com você</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Digite o nome do prato"
+          style={styles.input}
+          value={inputValue}
+          onChangeText={(text) => setInputvalue(text)}
+        />
+        <TouchableOpacity onPress={handleSearch}>
+          <Ionicons name="search" size={28} color="#4CBE6C" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'blue'
+    flex: 1,
+    backgroundColor: "#F3F9FF",
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: "#0e0e0e"
+  },
+  form: {
+    backgroundColor: '#fff',
+    width: '100%',
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#ececec",
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  input: {
+    width: "90%",
+    height: 54,
+    maxWidth: '90%'
   }
 })
