f46ff61aa36adc87b3777c6594619db00b088153
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-04-05 16:53:58 -0300

    detail component created

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,10 +1,44 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { Entypo } from '@expo/vector-icons'
+import { DataProps } from "../../components/FoodList/types";
+
+type RouteDataProps = {
+  data: DataProps
+}
+
+type RouteParamsProps = {
+  params: RouteDataProps
+  Readonly: { key: string; name: string; path?: string; }
+  key: any,
+  name: any
+}
 
 export default function Detail() {
+  const route = useRoute<RouteParamsProps>()
+  const navigation = useNavigation()
+
+  console.log(route)
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params?.data ? route.params?.data.name : "Detalhes da receita",
+      headerRight: () => (
+        <Pressable onPress={() => alert('Pressionou')}>
+          <Entypo
+            name="heart"
+            size={28}
+            color="#ff4141"
+          />
+        </Pressable>
+      )
+    })
+  }, [navigation, route.params?.data])
+
   return (
     <View style={styles.container}>
-      <Text>Página Detalhes da receita</Text>
+      <Text>{route.params?.data.name}</Text>
     </View>
   )
 }
